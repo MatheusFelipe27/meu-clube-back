f2bfa74e0a8a97a5e5a2f96e375710a8811112d6
Matheus Felipe <mfs5@cin.ufpe.br>
2023-09-10 22:27:14 -0300

    add subprocesses routes

--- src/processes/processes.ts
+++ src/processes/processes.ts
@@ -82,4 +82,67 @@ export default function Processes(fastify: FastifyInstance){
             res.status(500).send({ error: 'Erro interno do servidor.' });
         }
     });
+
+    //REFERENTE A SUBPROCESSOS
+
+    fastify.post<{ Params: { id: string }, Body: { detail: string } }>('/processos/:id/detalhes', async (req, res) => {
+        try {
+            const id = Number(req.params.id);
+            const { detail } = req.body;
+
+            if (isNaN(id) || id < 0 || id >= allProcesses.length || !detail) {
+                res.status(400).send({ error: 'ID não encontrado ou detalhe inválido.' });
+                return;
+            }
+
+            allProcesses[id].details?.push(detail);
+
+            res.status(201).send(allProcesses[id]);
+        } catch (error) {
+            res.status(500).send({ error: 'Erro interno do servidor.' });
+        }
+    });
+
+    fastify.delete<{ Params: { id: string, detailIndex: string } }>('/processos/:id/detalhes/:detailIndex', async (req, res) => {
+        try {
+            const id = Number(req.params.id);
+            const detailIndex = Number(req.params.detailIndex);
+
+            if (isNaN(id) || id < 0 || id >= allProcesses.length || isNaN(detailIndex) || detailIndex < 0 || detailIndex >= allProcesses[id].details.length) {
+                res.status(400).send({ error: 'ID ou índice de detalhe inválido.' });
+                return;
+            }
+
+            const removedDetail = allProcesses[id].details?.splice(detailIndex, 1);
+            res.status(200).send(removedDetail);
+        } catch (error) {
+            res.status(500).send({ error: 'Erro interno do servidor.' });
+        }
+    });
+
+    fastify.put<{ Params: { id: string, detailIndex: string }, Body: { detail: string } }>('/processos/:id/detalhes/:detailIndex', async (req, res) => {
+        try {
+            const id = Number(req.params.id);
+            const detailIndex = Number(req.params.detailIndex);
+            const { detail } = req.body;
+    
+            if (isNaN(id) || id < 0 || id >= allProcesses.length || isNaN(detailIndex) || detailIndex < 0 || !detail) {
+                res.status(400).send({ error: 'ID, índice de detalhe ou detalhe inválido.' });
+                return;
+            }
+    
+            const processToUpdate = allProcesses[id];
+    
+            if (!processToUpdate.details) {
+                processToUpdate.details = [];
+            }
+    
+            processToUpdate.details[detailIndex] = detail;
+    
+            res.status(200).send(processToUpdate);
+        } catch (error) {
+            res.status(500).send({ error: 'Erro interno do servidor.' });
+        }
+    });
+    
 }
